0e79e972ecfbc7f5ec8503d6497463009ed4af19
stkirk <shaanekirk@gmail.com>
2021-09-16 13:10:54 -0400

    set up Routes necessary to render components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import "./App.css";
 
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Nav from "./components/Nav";
+import Landing from "./components/Landing";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import PlantList from "./components/PlantList";
+import PlantForm from "./components/PlantForm";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Water My Plants</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          <Nav />
+        </header>
+        <h1>Water My Plants</h1>
+
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="/plants" component={PlantList} />
+          <Route path="/plantform" component={PlantForm} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
