6555620341108e093c4bca44d2163100fb816d60
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2021-09-19 18:06:21 -0700

    astarted login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,79 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+
+const initialData = {
+  username: "",
+  password: "",
+}
+
+const initialErrors = {
+  username: "",
+  password: "",
+}
+
 
 const Login = () => {
+
+  const [formData, setFormData] = useState(initialData);
+  const [errors, setErrors] = useState(initialErrors);
+  const [disabled, setDisabled] = useState(true);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    formSubmit();
+  }
+
+  const formSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("", formData)
+      .then(res => {
+        console.log(res)
+      })
+      .catch(err => console.log("Error, err"))
+  }
+
+  const onChange = (e) => {
+    e.preventDefault();
+    console.log(formData);
+    setFormData(initialData)
+  }
+
+  useEffect(() => {
+
+  })
+
   return (
-    <div className="login">
-      <h3>Login</h3>
-    </div>
+
+    <form>
+      <div className="login">
+        
+        <h3>Login</h3>
+          <label>username:</label>
+            <input
+              type = "text"
+              name = "username"
+              value = { formData.username }
+              onChange = { onChange }
+              />
+
+          <label>password:</label>
+          <input
+            type = "password"
+            name = "pasword"
+            value = { formData.password }
+            onChange = { onChange }
+          />
+
+          <button
+            type = "submit"
+            name = "submit"
+            disabled = {disabled}>submit</button>
+      </div>
+    </form>
+
   );
 };
+
 
 export default Login;
