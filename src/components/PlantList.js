c776aa719a98fe37f95321c84d2040663472baa5
stkirk <shaanekirk@gmail.com>
2021-09-22 15:47:04 -0400

    initialized plant state with fake data, setup PlantList container with Material UI and Masonry css, mapped over fake plant initial state

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,12 +1,39 @@
 import React from "react";
 
+import Container from "@material-ui/core/Container";
+import Plant from "./Plant";
+import Masonry from "react-masonry-css";
+import { connect } from "react-redux";
+
 //map over GET request from server to render individual <Plant /> components here
-const PlantList = () => {
+const PlantList = (props) => {
+  const breakpoints = {
+    default: 3,
+    1100: 2,
+    700: 1,
+  };
+
   return (
-    <div className="plantlist">
-      <h3>Plant List</h3>
-    </div>
+    <Container>
+      <Masonry
+        breakpointCols={breakpoints}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
+        {props.plants.map((plant) => (
+          <div key={plant.id}>
+            <Plant plant={plant} />
+          </div>
+        ))}
+      </Masonry>
+    </Container>
   );
 };
 
-export default PlantList;
+const mapStateToProps = (state) => {
+  return {
+    plants: state.plants,
+  };
+};
+
+export default connect(mapStateToProps, {})(PlantList);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,35 @@
 import { SET_LOGGED_IN } from "../actions";
 
+//Temporary data to get started
+const initialPlants = [
+  {
+    id: 1,
+    nickname: "Fred",
+    species: "Dragon Tree",
+    h2oFrequency: "Once a week",
+    description:
+      "Fred looks like a miniature palm tree, and is still alive after three years so lets count that as a win",
+  },
+  {
+    id: 2,
+    nickname: "Susan",
+    species: "Fiddle Leaf Fig",
+    h2oFrequency: "Twice per week",
+    description:
+      "Homeowners rely on Ficus lyrata to provide a fresh, modern twist in their decor, even in smaller spaces. Natural light is important for the large leaves to engage in photosynthesis, so place it close to a south or east-facing window. Allow soil to dry between waterings.",
+  },
+  {
+    id: 3,
+    nickname: "Bob",
+    species: "Spider Plant",
+    h2oFrequency: "Every two weeks",
+    description:
+      "Chlorophytum comosum has been a popular houseplant for decades, but this throwback plant is still relevant in any setting where you desire a low maintenance specimen that also purifies the air. The arching leaves make these plants great for pedestals or hanging baskets.",
+  },
+];
+
 const initialState = {
-  plants: [],
+  plants: initialPlants,
   hasAccount: false,
   isLoggedIn: true,
 };
